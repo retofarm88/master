1515bfda45eaebb7973a0c0e764a297b0843b326
retofarm88 <103258651+retofarm88@users.noreply.github.com>
2022-06-18 17:20:55 +0900

    Add files via upload

--- apps/config/database.js
+++ apps/config/database.js
@@ -1,20 +1,10 @@
 var mysql = require('mysql');
 
-var cdb_info = {
-  host: "retofarm88.cafe24.com",
-  user: "retofarm88",
-  password: "retofarm8!",
-  database: "retofarm88"
-};
-
-module.exports = {
-    init: function () {
-        return mysql.createConnection(db_info);
-    },
-    connect: function(conn) {
-        conn.connect(function(err) {
-            if(err) console.error('mysql connection error : ' + err);
-            else console.log('mysql is connected successfully!');
-        });
-    }
-}
+var con = mysql.createConnection({
+	host: "retofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com",
+	user: "retofarm88",
+	password: "retofarm8!",
+	database: "retofarm"
+});
+con.connect();
+module.exports = con;
