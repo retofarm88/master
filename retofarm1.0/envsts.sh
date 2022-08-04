1901d6f53a2cce1c03b901020fd1513f10c0cfb6
retofarm88 <retofarm88@github.com>
2022-08-04 21:52:51 +0900

    fromservertogit

--- retofarm1.0/envsts.sh
+++ retofarm1.0/envsts.sh
@@ -1,9 +1,9 @@
 mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envsts.sql
-mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envstsB.sql
-mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envstsC.sql
-mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envstsD.sql
-mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envsts2022002.sql
-mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envstsB2022002.sql
-mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envstsC2022002.sql
-mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envstsD2022002.sql
+#mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envstsB.sql
+#mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envstsC.sql
+#mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envstsD.sql
+#mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envsts2022002.sql
+#mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envstsB2022002.sql
+#mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envstsC2022002.sql
+#mysql  -hretofarmdb.cvbtmcuybt5k.ap-northeast-2.rds.amazonaws.com -uretofarm88 -pretofarm8! retofarm < /home/ec2-user/rtf_git/master/retofarm1.0/envstsD2022002.sql
 
